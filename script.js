7ef825e872ee234ca0c7db9b6da81237ff10d345
Eduardo <cojr.eduardo@gmail.com>
2024-04-02 16:19:19 +0200

    created a function called showAllPokemon. needs fixing

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 const pokemonContainer = document.querySelector(".pokemon-container");
 
-let pokemonLists;
+let pokemonList;
 
 //Read
-async function fetchPokemon() {}
+async function fetchPokemonList() {
+  const pokemonRequest = await fetch(
+    "https://pokeapi.co/api/v2/pokemon/?limit=50"
+  );
+  let pokemonResult = await pokemonRequest.json();
+  return pokemonResult;
+}
+
+//console.log(fetchPokemonList());
+
+async function fetchAndShowPokemon() {
+  try {
+    pokemonList = await fetchPokemonList();
+  } catch {
+    console.error("Unable to load pokemon list!", error);
+  }
+}
+
+console.log(fetchAndShowPokemon());
+
+function showAllPokemons() {
+  pokemonContainer.innerHTML = "";
+
+  pokemonList.forEach((pokeMonster, index) => {
+    const pokedexCard = document.createElement("div");
+
+    pokedexCard.innerHTML = `<h3>Name : ${pokeMonster.name}</h3><br>
+                               <h3>URL : ${pokeMonster.url}</h3>`;
+
+    pokemonContainer.appendChild(pokedexCard);
+  });
+}
+
+showAllPokemons();
