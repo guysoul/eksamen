148b20a38c6397d23085162f198689395293fa95
Eduardo <cojr.eduardo@gmail.com>
2024-04-09 20:15:36 +0200

    Created an async function to fetch 10 pokemons

--- script_pokebattle.js
+++ script_pokebattle.js
@@ -1,8 +1,59 @@
 const mainContainer = document.querySelector(".main-container");
-const myTeam = document.querySelector(".my-team");
-const enemyTeam = document.querySelector(".enemy-team");
+const pokemonList = document.querySelector(".pokemon-list");
+const battleGround = document.querySelector(".battle-ground");
 
-async function fetchMyTeamPokemon() {
-  const myTeamRequest = await fetch("https://pokeapi.co/api/v2/pokemon");
+mainContainer.style.display = "flex";
+mainContainer.style["justify-content"] = "space-between";
+mainContainer.style.height = "100%";
+mainContainer.style.width = "100%";
+
+pokemonList.style.display = "flex";
+pokemonList.style["align-content"] = "flex-start";
+pokemonList.style["flex-wrap"] = "wrap";
+pokemonList.style["row-gap"] = "10px";
+pokemonList.style["gap"] = "10px 20px";
+pokemonList.style.height = "100%";
+pokemonList.style.width = "30%";
+pokemonList.style["background-color"] = "lightblue";
+
+battleGround.style.display = "flex";
+battleGround.style["align-content"] = "flex-start";
+battleGround.style["flex-wrap"] = "wrap";
+battleGround.style["row-gap"] = "10px";
+battleGround.style["gap"] = "10px 20px";
+battleGround.style.height = "100%";
+battleGround.style.width = "67%";
+battleGround.style["background-color"] = "lightgreen";
+
+//Read
+async function fetchPokemonList() {
+  const myTeamRequest = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=10"
+  );
   let myTeamResult = await myTeamRequest.json();
+  return pokemonResult.results;
+}
+
+async function fetchPokemonInformation(pokeURL) {
+  const pokemonNumber = pokeURL.match(/\/(\d+)\/$/)[1];
+  const fetchPokemonResponse = await fetch(pokeURL);
+  const pokemonData = await fetchPokemonResponse.json();
+  let pokemonHP, pokemonAttack;
+
+  pokemonData.stats.forEach((pokeStats) => {
+    if (pokeStats.stat.name === "hp") {
+      pokemonHP = stat.base_stat;
+    } else if (pokeStats.stat.name === "attack") {
+      pokemonAttack = stat.base_stat;
+    }
+  });
+
+  return {
+    pokemonName: pokemonData.name,
+    pokemonImage: pokemonData.sprites.front_default,
+    pokemonTypes: pokemonData.types[0].type.name,
+    pokemonNumber: pokemonNumber,
+    pokemonHP: pokemonHP,
+    pokemonAttack: pokemonAttack,
+  };
 }
